56ac2893bfafe539cc7276af31858c39325134c2
Nightink <chceyes@gmail.com>
2014-07-10 14:11:32 +0800

    js hint `;`

--- README.md
+++ README.md
@@ -34,22 +34,22 @@ node.js find file cli
 
  **Lazy**
 
-  * index.js:5 var Lazy = require('lazy')
-  * index.js:73 var ly = new Lazy(fs.createReadStream(filePath))
+  * index.js:5 var Lazy = require('lazy');
+  * index.js:73 var ly = new Lazy(fs.createReadStream(filePath));
   * README.md:29 $ nff -p node_modules,.git -f Lazy
   * README.md:32 ========== node.js find {Lazy} list ==========
-  * README.md:35 *  **Lazy**  index.js:4 var Lazy = require('lazy')
-  * README.md:36 *  **Lazy**  index.js:65 var ly = new Lazy(fs.createReadStream(filePath))
+  * README.md:35 *  **Lazy**  index.js:4 var Lazy = require('lazy');
+  * README.md:36 *  **Lazy**  index.js:65 var ly = new Lazy(fs.createReadStream(filePath));
 
 
  **path**
 
-  * index.js:2 var path = require('path')
-  * index.js:22 var basename = path.basename(fileP)
-  * index.js:37 var filePath = path.join(basePath, filename)
-  * index.js:45 var fileExt = path.extname(filename)
+  * index.js:2 var path = require('path');
+  * index.js:22 var basename = path.basename(fileP);
+  * index.js:37 var filePath = path.join(basePath, filename);
+  * index.js:45 var fileExt = path.extname(filename);
   * bin/nff:8 .option('-p, --ignorePath [ignore file path]',
-  * bin/nff:9 'ignore file path(Default: .svn, .git, Examples: $ nff -p .git,bin)', String, '.svn,.git')
-  * README.md:21 -p, --ignorePath [ignore file path]  ignore file path(Default: .svn, .git, Examples: $ nff -p .git,bin)
+  * bin/nff:9 'ignore file path(Default: .svn, .git, Examples: $ nff -p .git,bin)', String, '.svn,.git');
+  * README.md:21 -p, --ignorePath [ignore file path]  ignore file path(Default: .svn, .git, Examples: $ nff -p .git,bin);
 
 ```
--- index.js
+++ index.js
@@ -1,95 +1,100 @@
 
-var fs = require('fs')
-var path = require('path')
-var util = require('util')
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
 
-var Lazy = require('lazy')
+var Lazy = require('lazy');
+var iconv = require('iconv-lite');
 
 module.exports = function(program) {
 
-  var cwdPath = process.cwd()
-  var filesPath = []
-  var ignoreFilePaths = program.ignorePath.split(',')
-  var ignores = program.ignore && program.ignore.split(',')
-  var findKeys = program.find.split(',')
+  var cwdPath = process.cwd();
+  var filesPath = [];
+  var ignoreFilePaths = program.ignorePath.split(',');
+  var ignores = program.ignore && program.ignore.split(',');
+  var findKeys = program.find.split(',');
 
   // 递归读取文件夹
   function readDir(basePath) {
 
-    var files = fs.readdirSync(basePath)
+    var files = fs.readdirSync(basePath);
 
     files = files.filter(function(fileP) {
 
-      var basename = path.basename(fileP)
+      var basename = path.basename(fileP);
 
       for(var i = 0, len = ignoreFilePaths.length; i < len; i++) {
 
         if(basename === ignoreFilePaths[i]) {
 
-          return false
+          return false;
         }
       }
 
-      return true
+      return true;
     })
 
     files.forEach(function(filename) {
 
-      var filePath = path.join(basePath, filename)
-      var stat = fs.statSync(filePath)
+      var filePath = path.join(basePath, filename);
+      var stat = fs.statSync(filePath);
 
       if(stat.isDirectory()) {
 
-        readDir(filePath)
+        readDir(filePath);
       } else {
 
-        var fileExt = path.extname(filename)
+        var fileExt = path.extname(filename);
 
         if(ignores) {
 
           for (var i = 0, len = ignores.length; i < len; i++) {
 
-            if('.' + ignores[i] === fileExt) return
+            if('.' + ignores[i] === fileExt) {
+              return
+            }
           }
         }
 
-        filesPath.push(filePath)
+        filesPath.push(filePath);
       }
     })
   }
 
-  readDir(cwdPath)
+  readDir(cwdPath);
 
-  var findWords = {}
+  var findWords = {};
 
   findKeys.forEach(function(findKeyword) {
 
-    findWords[findKeyword] = []
-  })
+    findWords[findKeyword] = [];
+  });
 
-  console.log('\n\n========== node.js find {%s} list ==========\n\n', findKeys)
+  console.log('\n\n========== node.js find {%s} list ==========\n\n', findKeys);
 
   function flowStream(filePath) {
 
-    var ly = new Lazy(fs.createReadStream(filePath))
-    var index = 0
+    var ly = new Lazy(fs.createReadStream(filePath));
+    var index = 0;
 
     ly.lines.forEach(function(bf) {
 
       // 空文件处理
-      if(!bf) return
+      if(!bf) {
+        return
+      }
 
-      var line = bf.toString()
-      index++
+      var line = bf.toString();
+      index++;
 
       findKeys.forEach(function(findKeyword) {
 
         if(line.indexOf(findKeyword) !== -1) {
 
           var formatStr = util.format('\033[1;36m  * \033[3;33m%s:%s\033[0m %s \033[0m',
-              filePath.substr(cwdPath.length + 1), index, line.replace(/^\s+/,""))
+              filePath.substr(cwdPath.length + 1), index, line.replace(/^\s+/,""));
 
-          findWords[findKeyword].push(formatStr)
+          findWords[findKeyword].push(formatStr);
         }
       })
     }).join(function() {
@@ -98,26 +103,26 @@ module.exports = function(program) {
 
         for(var findKey in findWords) {
 
-          var findWord = findWords[findKey]
-          if(!findWord || findWord.length === 0) return
+          var findWord = findWords[findKey];
+          if(!findWord || findWord.length === 0) {
+            return
+          }
 
-          console.log('\033[3;32m **%s** \033[0m\n', findKey)
+          console.log('\033[3;32m **%s** \033[0m\n', findKey);
 
           findWord.forEach(function(fdWord) {
 
-            console.log(fdWord)
-          })
+            console.log(fdWord);
+          });
 
-          console.log('\n')
+          console.log('\n');
         }
       } else {
 
-        flowStream(filesPath.pop())
+        flowStream(filesPath.pop());
       }
-
-    })
+    });
   }
 
-  flowStream(filesPath.pop())
-
-}
+  flowStream(filesPath.pop());
+};
